ec9ce0904850485de25c6b1aae8d682f4413ac3a
Matheus Hoy <matheus-hoy@hotmail.com>
2021-09-23 15:13:04 -0300

    finish fix and finish exercises

---
+++ desafio_css/source.js
@@ -0,0 +1,12 @@
+function foo(e) {
+    const article1 = document.getElementById("article1");
+    const article2 = document.getElementById("article2");
+    
+    if (article1.style.maxWidth == "") {
+      article1.style.maxWidth = "25%";
+      article2.style.maxWidth = "75%";
+    } else {
+      article1.style.maxWidth = "";
+      article2.style.maxWidth = "100%"
+    }
+  }
