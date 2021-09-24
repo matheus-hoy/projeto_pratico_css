59b86c56acac2e710137c3423b813739fd9781b3
Matheus Hoy <matheus-hoy@hotmail.com>
2021-09-24 11:11:23 -0300

    finish last exercise

--- desafio_css/source.js
+++ desafio_css/source.js
@@ -1,12 +1,22 @@
 function foo(e) {
-    const article1 = document.getElementById("article1");
-    const article2 = document.getElementById("article2");
-    
-    if (article1.style.maxWidth == "") {
-      article1.style.maxWidth = "25%";
-      article2.style.maxWidth = "75%";
-    } else {
-      article1.style.maxWidth = "";
-      article2.style.maxWidth = "100%"
-    }
+  const article1 = document.getElementById("article1");
+  const article2 = document.getElementById("article2");
+
+  if (article1.style.maxWidth == "") {
+    article1.style.maxWidth = "25%";
+    article2.style.maxWidth = "75%";
+  } else {
+    article1.style.maxWidth = "";
+    article2.style.maxWidth = "100%";
   }
+}
+
+
+function openModal() {
+  const modal = document.getElementById("modal");
+  modal.style.display = "block";
+}
+function closeModal() {
+  const modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
